30187ef05d73cbb9ef0e96361470321706abfc0f
Luz Elena Torres Talaverano <luzelena.torres@unmsm.edu.pe>
2022-12-23 19:11:44 -0500

    add

--- apps/model1.py
+++ apps/model1.py
@@ -2,16 +2,16 @@ import streamlit as st
 #import tweepy
 #from textblob import TextBlob
 #from wordcloud import WordCloud, ImageColorGenerator
-from nltk.tokenize import WordPunctTokenizer
+#from nltk.tokenize import WordPunctTokenizer
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import re
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
-import nltk
-from nltk.corpus import stopwords
-nltk.download("stopwords")
+#import nltk
+#from nltk.corpus import stopwords
+#nltk.download("stopwords")
 def app():
     st.header('Análisis de sentimientos de tweets sobre Pedro Castillo')
     import snscrape.modules.twitter as sntwitter
